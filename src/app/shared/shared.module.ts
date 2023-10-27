039334d100b7b974b127aacc7c7b8cc1bdd305b1
Daniel Restrepo <jdrestrepo@unitecnica.net>
2023-10-27 14:37:43 -0500

    Finish navbar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    NavbarComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    HomePageComponent
+    HomePageComponent,
+    NavbarComponent
   ]
 })
 export class SharedModule { }
