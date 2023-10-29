7a137c5c9a8d05fc945871a5149fe6a1cdcf0455
Daniel Restrepo <jdrestrepo@unitecnica.net>
2023-10-28 23:53:51 -0500

    Get cocktail info and begin with cocktail page interface

--- src/app/cocktails/pages/cocktail-page/cocktail-page.component.ts
+++ src/app/cocktails/pages/cocktail-page/cocktail-page.component.ts
@@ -1,34 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CocktailsService } from '../../services/cocktails.service';
+import { Cocktail, Drinks } from '../../interfaces/cocktail';
+
 
 @Component({
   selector: 'app-cocktail-page',
   templateUrl: './cocktail-page.component.html',
-  styles: [
-  ]
+  styleUrls: ['./cocktail-page.component.css']
 })
 export class CocktailPageComponent implements OnInit {
+
+ 
+
+  public cocktail?: Cocktail;
+  public drink?: Drinks;
+  public name: string = "";
+  public urlImg: string= "";
+  public ingridients:[] = []; 
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private cocktailService: CocktailsService,
-    private router: Router  
+    private router: Router
   ) { }
 
   ngOnInit(): void {
     this.activatedRoute.params
       .subscribe(({ id }) => {
         this.searchCocktail(id);
-      })  
+      })
   }
 
   searchCocktail(id: number): void {
     this.cocktailService.searchCocktailById(id)
       .subscribe(cocktail => {
-          if (!cocktail.drinks){
-            this.router.navigateByUrl('');
-            return;
-          }
+        if (!cocktail.drinks) {
+          this.router.navigateByUrl('');
+          return;
+        }
+        this.cocktail = cocktail;
+        this.drink = cocktail.drinks[0]
+        console.log(this.drink)
+        this.name = this.drink.strDrink;
+        this.urlImg = this.drink.strDrinkThumb;
+  
       });
   }
 
